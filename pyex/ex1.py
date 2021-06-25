a1f8620ef2660088eea7aa5ed1007f70f352e7c6
Vee Satayamas <vee.sa@protonmail.com>
2021-06-25 22:34:31 +0700

    Add examples

---
+++ pyex/ex1.py
@@ -0,0 +1,20 @@
+import pyarrow as pa
+from pyarrow import csv
+
+FRAME_SIZE = 300
+RATIO = 11.1
+
+def is_fake_max_age(age, i):
+    is_max = True
+    for j in range(i + 1, min(i + 1 + FRAME_SIZE, len(age))):
+        age_j = age[j].as_py()
+        age_i = age[i].as_py()
+        if age_j is not None and age_i is not None and age[j].as_py() > age[i].as_py() * RATIO:
+            is_max = False
+            break
+    return age
+
+tab = csv.read_csv("../confirmed-cases.csv")
+age = tab["age"]
+max_age_col = [is_fake_max_age(age, i) for i in range(len(age))]
+print(len(max_age_col), max_age_col[10000])
