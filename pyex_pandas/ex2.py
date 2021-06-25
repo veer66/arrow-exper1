d1057e1cb3e39edb6bbcb6e080dfa3ab26d302ac
Vee Satayamas <vee.sa@protonmail.com>
2021-06-25 23:39:58 +0700

    Optimize Python version and add Python + Pandas version

--- pyex_pandas/ex2.py
+++ pyex_pandas/ex2.py
@@ -1,5 +1,4 @@
-import pyarrow as pa
-from pyarrow import csv
+import pandas as pd
 
 FRAME_SIZE = 300
 RATIO = 11.1
@@ -7,14 +6,15 @@ RATIO = 11.1
 def is_fake_max_age(age, i):
     is_max = True
     for j in range(i + 1, min(i + 1 + FRAME_SIZE, len(age))):
-        age_j = age[j].as_py()
-        age_i = age[i].as_py()
+        age_j = age[j]
+        age_i = age[i]
         if age_j is not None and age_i is not None and age_j > age_i * RATIO:
             is_max = False
             break
     return age
 
-tab = csv.read_csv("../confirmed-cases.csv")
+tab = pd.read_csv("../confirmed-cases.csv")
 age = tab["age"]
 max_age_col = [is_fake_max_age(age, i) for i in range(len(age))]
 print(len(max_age_col), max_age_col[10000])
+
